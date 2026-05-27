d84b808886efb113e8b70ea88a774ec5a572a5f9
Mohammad Raihan <108555328+Raihanhn@users.noreply.github.com>
2026-05-27 20:41:45 +0600

    left sidebar created

--- app/(core)/layout.tsx
+++ app/(core)/layout.tsx
@@ -1,15 +1,18 @@
-import { Children } from "react";
+import { LeftSidebar } from "@/components/layout/Left-sidebar";
+import { Navbar } from "@/components/layout/Navbar";
 
-
-export default async function CoreGroupLayout({children}: {children: React.ReactNode}) {
-    return (
-        <>
-    <Navbar/>
-    <div>
-        <LeftSidebar/>
+export default async function CoreGroupLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <LeftSidebar />
         <div>{children} </div>
-    </div>
-     </>
-    )
-    
+      </div>
+    </>
+  );
 }
---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,10 @@
+export function Navbar() {
+    return (
+        <nav className="w-full h-16 bg-primary text-primary-foreground flex items-center justify-between px-4">
+            <div className="text-lg font-bold">Reddit Clone</div>
+            <div>
+                {/* Add navigation links or user profile here */}
+            </div>
+        </nav>
+    )
+}

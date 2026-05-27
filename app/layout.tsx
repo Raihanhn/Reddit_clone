2a9f59ffc8de199abb96e039f678d4e88a796485
Mohammad Raihan <108555328+Raihanhn@users.noreply.github.com>
2026-05-27 20:22:28 +0600

    neon-auth-ui-provider created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { NeonAuthProviders } from "@/providers/neon-auth-ui-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,12 +23,19 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+
+
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`dark ${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-background text-foreground ">
+        <NeonAuthProviders>
+        {children}
+        </NeonAuthProviders>
+        </body>
     </html>
   );
 }

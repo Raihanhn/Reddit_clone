df6ac623bd2c014ad361022a2419ba10c62e2ac4
Mohammad Raihan <108555328+Raihanhn@users.noreply.github.com>
2026-05-29 23:46:47 +0600

    navbar content updated

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -27,6 +27,9 @@ export function Navbar() {
              className="w-full rounded-md border-0 bg-secondary text-sm ring-1 ring-input focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 pl-10"
              />
         </div>
+
+        <div className=""></div>
+
       </div>
     </header>
   );

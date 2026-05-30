0d54039482ae1ab6e4864a0035b53611897d0095
Mohammad Raihan <108555328+Raihanhn@users.noreply.github.com>
2026-05-30 21:10:32 +0600

    navbar content completed

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,11 +1,13 @@
 import { Search } from "lucide-react";
 import Link from "next/link";
 import { Input } from "../ui/input";
+import { buttonVariants } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 export function Navbar() {
   return (
-    <header>
-      <div>
+    <header className=" sticky top-0 z-50 border-b border-border bg-background/90 backdrop-blur-md ">
+      <div className="mx-auto flex h-14 max-w-[1200px] items-center gap-4 px-4">
         <Link
           href="/"
           className="flex items-center gap-2 font-semibold tracking-tight text-foreground"
@@ -20,7 +22,7 @@ export function Navbar() {
           <span className="text-lg">Threadly</span>
         </Link>
 
-        <div>
+        <div className="relative mx-auto hidden max-w-xl flex-1 md:block">
           <Search className=" pointer-events-none absolute left-3 top-1/2 size-4 -translate-y-1/2 text-muted-foreground " />
           <Input
             readOnly
@@ -29,11 +31,11 @@ export function Navbar() {
           />
         </div>
 
-        <div className="">
-          <Link href="/auth/sign-in" className="">
+        <div className="ml-auto flex items-center gap-2">
+          <Link href="/auth/sign-in" className={cn(buttonVariants({variant: "ghost", size: "default"}))}>
             Log In
           </Link>
-           <Link href="/auth/sign-up" className="">
+           <Link href="/auth/sign-up" className={cn(buttonVariants({ variant: "default" }))}>
             Sign Up
           </Link>
         </div>

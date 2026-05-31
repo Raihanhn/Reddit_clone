d4d4d949ac1e773fb6863f38bd658436c8e8ab53
Mohammad Raihan <108555328+Raihanhn@users.noreply.github.com>
2026-05-31 22:19:39 +0600

    neon auth process added

---
+++ app/auth/[pathname]/page.tsx
@@ -0,0 +1,9 @@
+import { AuthView } from "@neondatabase/auth/react";
+
+export default async function ()  {
+ return(
+    <div className="">
+        <div className=""> <AuthView/> </div>
+    </div>
+ )
+}
